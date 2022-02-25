0754f30f6b078cc17d6528cbd3dfbb26f94fcdb9
StenlySamberi <i.connect@outlook.co.idclear>
2022-02-26 05:28:29 +0700

    add sub total

--- app/Http/Controllers/ControllerMitra.php
+++ app/Http/Controllers/ControllerMitra.php
@@ -62,8 +62,8 @@ class ControllerMitra extends Controller
             [
             'mitra'  =>  ModelMitra::mitras()->get(),
             'income' =>  KasPemasukan::mitras()->get(),
-            'spent'  =>  ModelPengeluaran::mitras()->get(),
-            'total_kas' => KasPemasukan::total_dana()
+            'spent'  =>  ModelPengeluaran::mitras()->get()
+           
             ]
         ]);
           
